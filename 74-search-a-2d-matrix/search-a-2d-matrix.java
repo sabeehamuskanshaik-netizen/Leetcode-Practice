22e9eddf24f86ff734059285e361236861999a5f
Sabeeha Muskan Shaik <sabeehamuskanshaik@gmail.com>
2026-08-11 00:28:09 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (44.42%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,43 +1,36 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-
-        // Binary search to find the possible row
-        int l = 0;
-        int h = rows - 1;
-
-        while (l <= h) {
-            int mid = l + (h - l) / 2;
-
-            if (target >= matrix[mid][0] && target <= matrix[mid][cols - 1]) {
-                
-                // Binary search inside this row
-                int left = 0;
-                int right = cols - 1;
-
-                while (left <= right) {
-                    int m = left + (right - left) / 2;
-
-                    if (matrix[mid][m] == target) {
-                        return true;
-                    } else if (matrix[mid][m] < target) {
-                        left = m + 1;
-                    } else {
-                        right = m - 1;
-                    }
-                }
-
-                return false;
-            } 
-            else if (target > matrix[mid][cols - 1]) {
-                l = mid + 1;
-            } 
-            else {
-                h = mid - 1;
+    public boolean searchInRow(int[][] mat,int c,int row,int target){
+        int l=0;
+        int h=c-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(mat[row][mid]==target){
+                return true;
+            }else if(mat[row][mid]<target){
+                l=mid+1;
+            }else{
+                h=mid-1;
             }
         }
-
         return false;
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int r=matrix.length;
+        int c=matrix[0].length;
+       int l=0;
+        int h=r-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(target>=matrix[mid][0] && target<=matrix[mid][c-1]){
+                //search inside this one
+                return searchInRow(matrix,c,mid,target);
+            }else if(target>matrix[mid][c-1]){
+                l=mid+1;
+            }else if(target<matrix[mid][0]){
+                h=mid-1;
+            }
+        }
+        return false;
+        
     }
 }
